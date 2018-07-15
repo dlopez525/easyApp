829565d478e77b6f08909e6dbf76b2c564e515ee
Daniel López Sánchez <dlopezs2009@hotmail.com>
2018-07-15 14:38:59 -0500

    home principal listo

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -4,3 +4,10 @@ var toggleMenu = document.getElementById('toggle-menu'),
 toggleMenu.addEventListener('click', function() {
     nav.classList.toggle('showMenuBar');
 });
+
+var toggleOptions = document.getElementById('toggle-tools'),
+    div = document.getElementById('tools__options');
+
+toggleOptions.addEventListener('click', function() {
+    div.classList.toggle('showToolsOptions');
+});
